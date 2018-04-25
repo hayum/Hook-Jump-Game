6679e6e036c5a0d40686be776a792aa74b2013a4
Shuang <shuannnng@Shuangs-MacBook-Pro.local>
2018-04-25 02:37:21 -0400

    add unity project

---
+++ Assets/Scripts/CrocodileControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrocodileControl : MonoBehaviour {
+
+	Animator animCro, animH;
+	bool isEnd,timeOut;
+	public int freq=1;
+	GameObject Hook, empty;
+	public GameObject cvs,cvs2;
+	int translation=0, splitTime;
+	// Use this for initialization
+	void Start () {
+		Random.InitState(22);
+		animCro=GetComponent<Animator>();
+		animH=GameObject.Find("Hook").GetComponent<Animator>();
+		empty=GameObject.Find("emp");
+		//cvs=GameObject.Find("Canvas");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Time.frameCount%50==0 && !isEnd){
+			int randomNumber=Random.Range(0,50);
+			//Debug.Log(randomNumber);
+			if(randomNumber%freq==0)
+			{
+				animCro.SetBool("Open",true);
+				timeOut=false;
+				splitTime=0;
+			}
+			else {
+				animCro.SetBool("Open",false);
+			}
+		}
+		if(animCro.GetBool("Open") && !isEnd && !timeOut){
+			//float translation = Time.deltaTime * 10;
+			//empty.transform.Translate(0, 0, translation);
+
+			translation++;
+			if(translation<50){
+				if(animH.GetBool("Jump")){
+					timeOut=true;
+					translation=0;
+				}
+			}
+			else{
+				isEnd=true;
+				Debug.Log("fail");
+				animCro.SetBool("Open",false);
+				cvs.SetActive(true);
+			}
+		}
+		if(!animCro.GetBool("Open") && !isEnd && animH.GetBool("Jump")){
+			splitTime++;
+			if(splitTime>60){
+			isEnd=true;
+			Debug.Log("fail2");
+			animCro.SetBool("Open",false);
+			cvs2.SetActive(true);
+			}
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/SimpleSerial.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+public class SimpleSerial : MonoBehaviour {
+
+	private SerialPort serialPort = null;
+	private String portName = "/dev/cu.usbmodem1411";
+	private int baudRate = 9600;
+	private int readTimeOut = 100;
+
+	private string serialInput;
+
+	bool programActive = true;
+	Thread thread;
+
+	Animator anim;
+
+	void Start()
+	{   
+		anim=GetComponent<Animator>();
+		try
+		{
+			serialPort = new SerialPort();
+			serialPort.PortName = portName;
+			serialPort.BaudRate = baudRate;
+			serialPort.ReadTimeout = readTimeOut;
+			serialPort.Open();
+		}
+		catch (Exception e)
+		{
+			Debug.Log(e.Message);
+		}
+		thread = new Thread(new ThreadStart(ProcessData));
+		thread.Start();
+	}
+
+	void ProcessData()
+	{
+		Debug.Log("Thread: Start");
+		while (programActive)
+		{
+			try
+			{
+				serialInput = serialPort.ReadLine();
+			}
+			catch (TimeoutException)
+			{
+
+			}
+		}
+		Debug.Log("Thread: Stop");
+	}
+
+	void Update()
+	{
+		if (serialInput != null)
+		{
+			string[] strEul = serialInput.Split(',');
+			if (strEul.Length > 0)
+			{
+				float potA = float.Parse(strEul[0]);
+				Debug.Log(potA);
+				//float potB = float.Parse(strEul[1]);
+				//				potA = (potA - 512) / 512;
+				//				potB = (potB - 512) / 512;
+				//				this.transform.rotation = Quaternion.Euler(new Vector3(90.0f * potA, 90.0f * potB, 0f));
+				//				if (int.Parse(strEul[2]) != 0)
+				//				{
+				//					this.GetComponent<MeshRenderer>().enabled = true;
+				//				}
+				//				else
+				//				{
+				//					this.GetComponent<MeshRenderer>().enabled = false; 
+				//				}
+				if(potA<120){
+					anim.SetBool("Jump",true);
+					//					if(potB==2){
+					//						anim.speed = 2f;
+					//					}
+					//					else{
+					//						anim.speed = 0.5f;
+					//					}
+					//anim.speed = potB;
+				}
+				else{
+					anim.speed = 1f;
+					anim.SetBool("Jump",false);
+				}
+				//Debug.Log(potA);
+				//Debug.Log("--------");
+				//Debug.Log(potB);
+
+			}
+		}
+	}
+
+	public void OnDisable()
+	{
+		programActive = false;
+		if (serialPort != null && serialPort.IsOpen)
+			serialPort.Close();
+	}
+}
