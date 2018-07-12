e25be3130c769be6b886f7f15b109783ec99fe44
hayum <shuannnng@gmail.com>
2018-07-11 21:17:46 -0400

    Update SimpleSerial.cs

--- Assets/Scripts/SimpleSerial.cs
+++ Assets/Scripts/SimpleSerial.cs
@@ -65,27 +65,9 @@ public class SimpleSerial : MonoBehaviour {
 			{
 				float potA = float.Parse(strEul[0]);
 				Debug.Log(potA);
-				//float potB = float.Parse(strEul[1]);
-				//				potA = (potA - 512) / 512;
-				//				potB = (potB - 512) / 512;
-				//				this.transform.rotation = Quaternion.Euler(new Vector3(90.0f * potA, 90.0f * potB, 0f));
-				//				if (int.Parse(strEul[2]) != 0)
-				//				{
-				//					this.GetComponent<MeshRenderer>().enabled = true;
-				//				}
-				//				else
-				//				{
-				//					this.GetComponent<MeshRenderer>().enabled = false; 
-				//				}
+			
 				if(potA<120){
 					anim.SetBool("Jump",true);
-					//					if(potB==2){
-					//						anim.speed = 2f;
-					//					}
-					//					else{
-					//						anim.speed = 0.5f;
-					//					}
-					//anim.speed = potB;
 				}
 				else{
 					anim.speed = 1f;
